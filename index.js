495cf7ca31a20907979a0940289f9c93feb9d019
Rohit <rohitkarnawal.dev@gmail.com>
2026-06-30 10:17:43 +0530

    Initial Express server setup with MongoDB connection

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+main().then( () => {
+    console.log("connected tp db");
+}).catch((err) => {
+    console.log(err);
+});
+
+async function main(){
+    await mongoose.connect(MONGO_URL)
+}
+
+app.get("/", (req, res) => {
+    res.send("hi im root");
+})
+
+app.listen(8080, ()=> {
+    console.log("Listening on port 8080");
+});
