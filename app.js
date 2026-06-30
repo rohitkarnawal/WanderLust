9f71fb64864244ce24b0fea8658bb250adc027e8
Rohit <rohitkarnawal.dev@gmail.com>
2026-06-30 13:42:39 +0530

    Add route to create new listings

--- app.js
+++ app.js
@@ -17,15 +17,36 @@ async function main(){
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
     res.send("hi im root");
 })
 
+//index route
 app.get("/listings", async (req, res) => {
    const allListings = await Listing.find({});
     res.render("listings/index.ejs", {allListings});
 });
+
+// new route 
+app.get("/listings/new", (req, res) => {
+    res.render("listings/new.ejs");
+})
+
+app.post("/listings", async (req, res) => {
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
+})
+
+//show route
+app.get("/listings/:id", async (req, res) => {
+    let {id} = req.params;
+   const listing = await Listing.findById(id);
+   res.render("listings/show.ejs", {listing});
+});
+
 
 
 // app.get("/testListing", async (req, res) => {
