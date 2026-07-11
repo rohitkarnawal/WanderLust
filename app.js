49ab8c619c482ef1bc7e9683303765e706fc0296
Rohit <rohitkarnawal.dev@gmail.com>
2026-07-11 10:13:37 +0530

    Prepare project for deployment

--- app.js
+++ app.js
@@ -30,8 +30,7 @@ const listingRouter = require("./routes/listing.js");
 const userRouter = require("./routes/user.js");
 const reviewRouter = require("./routes/review.js");
 
-// Db connection
-// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
 const dbUrl = process.env.ATLASDB_URL;
 
 main()
@@ -58,7 +57,7 @@ app.use(express.static(path.join(__dirname, "/public")));
 const store = MongoStore.create({
   mongoUrl : dbUrl,
   crypto: {
-    secret: "mysupersecretcode",
+    secret: process.env.SECRET,
     touchAfter: 24 * 3600, 
   }
 });
@@ -70,7 +69,7 @@ store.on("error" , () => {
 // Session for cookies
 const sessionOptions = {
   store,
-  secret: "mysupersecretcode",
+  secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true,
   cookie: {
