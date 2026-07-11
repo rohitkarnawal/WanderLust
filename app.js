cfad8bf0398d06de8cc44d3832a3d35ec6cc36a3
Rohit <rohitkarnawal.dev@gmail.com>
2026-07-11 14:37:31 +0530

    Make navbar and filters fully responsive

--- app.js
+++ app.js
@@ -99,11 +99,13 @@ passport.deserializeUser(User.deserializeUser());
 
 //Flash messages
 app.use((req, res, next) => {
+ 
+
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   res.locals.currUser = req.user;
   next();
-})
+});
 
 
 //Listing and Review
